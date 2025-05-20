f7c6c768461485c64360751acefcab09b02a7af0
Braden Everson <bradeneverson@gmail.com>
2025-05-20 10:32:36 -0500

    Init

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "bfjit",
+        .root_source_file = b.path("main.zig"),
+        .target = b.graph.host,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the BF jit interprete");
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    run_step.dependOn(&run_cmd.step);
+}
