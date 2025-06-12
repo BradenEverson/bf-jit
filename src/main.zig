7911856df71c0fce5f61346dc52d66dc3e3f96e5
BradenEverson <bradeneverson@gmail.com>
2025-06-12 05:44:14 -0500

    Grab a file

---
+++ src/main.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn exit_err(msg: []const u8) noreturn {
+    std.debug.print("{s}\n", .{msg});
+    std.process.exit(1);
+}
+
+pub fn main() void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    defer {
+        _ = gpa.deinit();
+    }
+
+    var args = std.process.args();
+    _ = args.skip();
+
+    const path = args.next() orelse exit_err("Include a file please");
+
+    const file = std.fs.cwd().openFile(path, .{}) catch exit_err("File does not exist >:(");
+    defer file.close();
+
+    const buf = file.readToEndAlloc(allocator, 1024) catch exit_err("Did you really try running this on a system with less than 1024 bytes of memory?");
+    defer allocator.free(buf);
+
+    std.debug.print("Buf:\n{s}\n", .{buf});
+}
