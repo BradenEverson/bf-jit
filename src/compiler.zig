10e9b28319108265387c49190f1a1b91cfb14b1e
BradenEverson <bradeneverson@gmail.com>
2025-06-19 05:30:12 -0500

    Looping works!

--- src/compiler.zig
+++ src/compiler.zig
@@ -34,7 +34,7 @@ pub const CompiledRuntime = struct {
             \\
         ) catch exit_err("Failed to write :(");
 
-        for (self.commands) |cmd| {
+        for (self.commands, 0..) |cmd, i| {
             switch (cmd.kind) {
                 .left => {
                     const instr = std.fmt.allocPrint(allocator, "sub rbx, {d}\n", .{cmd.extra}) catch exit_err("Failed to format string");
@@ -66,6 +66,28 @@ pub const CompiledRuntime = struct {
                     for (0..cmd.extra) |_| {
                         _ = self.fd.write(instr) catch exit_err("Failed to write ;(");
                     }
+                },
+
+                .while_start => {
+                    const instr = std.fmt.allocPrint(allocator,
+                        \\open{d}:
+                        \\cmp byte [rbx], 0
+                        \\je close{d}
+                        \\
+                    , .{ i, cmd.extra }) catch exit_err("Failed to format string");
+
+                    _ = self.fd.write(instr) catch exit_err("Failed to write ;(");
+                },
+
+                .while_end => {
+                    const instr = std.fmt.allocPrint(allocator,
+                        \\cmp byte [rbx], 0
+                        \\jne open{d}
+                        \\close{d}:
+                        \\
+                    , .{ cmd.extra, i }) catch exit_err("Failed to format string");
+
+                    _ = self.fd.write(instr) catch exit_err("Failed to write ;(");
                 },
 
                 else => @panic("not implemented"), // unimplemented for now
